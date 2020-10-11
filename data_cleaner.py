44d39ba3e554175e54e75c9cdbfbf805428fe832
Sulav Khadka <khadkasulav@gmail.com>
2020-10-11 16:51:29 -0400

    Seperated the generator pipeline into a SimpleDataPipeline class

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,47 +1,16 @@
-import glob
+from string import punctuation
 import ijson
-from simple_item_store import SimpleItemStore
 from tqdm import tqdm
-
-
-class NepaliCorpusPreProcessor(object):
-    """ Original Code: https://brett.is/writing/about/generator-pipelines-in-python/ 
-    """
-
-    def __init__(self, data_folder_path):
-        self._data_folder_path = glob.glob(data_folder_path)
-        self._storage = dict()
-        self._storage_options = {type(set()), type(list()), type(str()), type(SimpleItemStore())}
-        self._filters = []
-
-
-    def add_filter(self, new_filter, storage=None):
-        if callable(new_filter):
-            self._filters.append(new_filter)
-            if storage is not None:
-                if type(storage) in self._storage_options:
-                    self._storage[new_filter.__name__] = storage
-
-
-    def process(self):
-        # this is the pattern for creating a generator
-        # pipeline, we start with a generator then wrap
-        # each consecutive generator with the pipeline itself
-        pipeline = self._data_folder_path
-        for new_filter in self._filters:
-            storage = self._storage.get(new_filter.__name__)
-            if storage is not None:
-                pipeline = new_filter(pipeline, storage)
-            else:
-                pipeline = new_filter(pipeline)
-        return pipeline
-
+from simple_item_store import SimpleItemStore
+from simple_data_pipeline import SimpleDataPipeline
+from nepali_tokenizer import NepaliTokenizer
+from data.language_data.stopwords import stopwords
+from data.language_data.alphabhet_and_characters import alphabhet
 
 
 def load_news_data(file_path_list):
-
     for file_path in file_path_list:
-        print (f"Working with: {file_path}")
+        print (f"\nWorking with: {file_path}")
 
         with open(file_path, "rb") as file:
             filter = "item.sentences.item"
@@ -50,21 +19,30 @@ def load_news_data(file_path_list):
                 yield item
 
 
+def sanitize_sentence(sentences, tokenizer, word_filter_set):
+    for sentence in sentences:
+        tokenized_sentence = tokenizer.word_tokenize(sentence)
+        yield " ".join([word for word in tokenized_sentence if word not in word_filter_set])
+        
+
 def unique_sentences(sentences, storage):
-    with open('data/sanitized/unique_sentences.txt', 'w') as file:
+    with open('data/sanitized/unique_sentences_trial.txt', 'w') as file:
         for sentence in sentences:
             if sentence not in storage:
                 storage.add(sentence)
                 file.write(f"{sentence}\n")
-            
             yield sentence
 
 
-processor = NepaliCorpusPreProcessor("data/unsanitized/*.json")
+processor = SimpleDataPipeline("data/unsanitized/*.json")
 
 # this is the order we want the functions to run
-processor.add_filter(load_news_data)
-processor.add_filter(unique_sentences, storage=SimpleItemStore())
+processor.add_filter(new_filter=load_news_data)
+
+filter_words = [*stopwords, *alphabhet['number'], *alphabhet['punctuation']]
+processor.add_filter(new_filter=sanitize_sentence, args=[NepaliTokenizer(), set(filter_words)])
+
+processor.add_filter(new_filter=unique_sentences, storage=SimpleItemStore())
 
 # process() returns the generator pipeline
 counter = 0
---
+++ simple_data_pipeline.py
@@ -0,0 +1,49 @@
+import glob
+from simple_item_store import SimpleItemStore
+from simple_key_value_store import SimpleKeyValueStore
+
+
+class SimpleDataPipeline(object):
+    """ Original Code: https://brett.is/writing/about/generator-pipelines-in-python/ 
+    """
+
+    def __init__(self, data_folder_path):
+        self._data_folder_path = glob.glob(data_folder_path)
+        self._storage = dict()
+        self._storage_options = {type(set()), type(list()), type(str()), type(SimpleItemStore()), type(SimpleKeyValueStore())}
+        self._filters = []
+
+
+    def add_filter(self, *, args=None, new_filter=None, storage=None):
+        if callable(new_filter):
+            if args:
+                self._filters.append((new_filter, args))
+            else:
+                self._filters.append((new_filter, None))
+            
+            if storage is not None:
+                if type(storage) in self._storage_options:
+                    self._storage[new_filter.__name__] = storage
+                else:
+                    #TODO: Pass back message that informs the user to use one of the options from self._storage
+                    pass
+
+
+    def process(self):
+        # this is the pattern for creating a generator
+        # pipeline, we start with a generator then wrap
+        # each consecutive generator with the pipeline itself
+        pipeline = self._data_folder_path
+        for new_filter, args in self._filters:
+            storage = self._storage.get(new_filter.__name__)
+            if args:
+                if storage is not None:
+                    pipeline = new_filter(pipeline, storage, *args)
+                else:
+                    pipeline = new_filter(pipeline, *args)
+            else:
+                if storage is not None:
+                    pipeline = new_filter(pipeline, storage)
+                else:
+                    pipeline = new_filter(pipeline)
+        return pipeline
